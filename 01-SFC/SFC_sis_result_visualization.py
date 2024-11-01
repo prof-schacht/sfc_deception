f5ae73f0bbe0866bb5598bad1cd0eda24e17dcd1
TarasKutsyk <tkutsyk2103@gmail.com>
2024-11-01 11:34:49 +0000

    Ablation experiment finished

--- 01-SFC/SFC_sis_result_visualization.py
+++ 01-SFC/SFC_sis_result_visualization.py
@@ -541,7 +541,7 @@ def create_activation_visualization_plotly(data, K=10, group_padding=0.5, layer_
 
 # %%
 aggregation_type = AttributionAggregation.ALL_TOKENS
-THRESHOLD = 0.02
+THRESHOLD = 0.01
 NODES_PREFIX = ''# 'resid_saes_128k'
 
 def get_nodes_fname(truthful_nodes=True, nodes_prefix=NODES_PREFIX):
@@ -606,7 +606,7 @@ deceptive_nodes_scores = load_dict(deceptive_nodes_fname)
 deceptive_nodes_scores.keys()
 
 # %%
-THRESHOLD=0.02
+THRESHOLD=0.01
 
 deceptive_nodes_scores, total_components = get_contributing_components(deceptive_nodes_scores, THRESHOLD)
 print(f"Total contributing components: {total_components}")
